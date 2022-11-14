e15dd2fa5caccd55c4803f78eef65eeb2610e10f
Akinrolabu Lolade <l.akinrolabu@csdcconsulting.com>
2022-11-14 16:55:21 +0100

    feat: serverless framework

--- serverless/src/lambda/http/getGroups.ts
+++ serverless/src/lambda/http/getGroups.ts
@@ -7,7 +7,6 @@ const docClient = new AWS.DynamoDB.DocumentClient()
 const groupsTable = process.env.GROUPS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  console.log('Processing event: ', event)
 
   const result = await docClient.scan({
     TableName: groupsTable
